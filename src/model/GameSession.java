51c67b9f5e62c996986e220c5c07477fe69a3d5a
hadeel-hamodi <abo.alhlo.hadeel@gmail.com>
2025-11-22 23:34:21 +0200

    GameSession skeleton
    new class to use its fun'

---
+++ src/model/GameSession.java
@@ -0,0 +1,12 @@
+package model;
+
+public class GameSession {
+	public void decreaseLives() {
+        // יבוצע ע"י מפתח 3
+    }
+
+    public void updateScore(int points) {
+        // יבוצע ע"י מפתח 3
+    }
+
+}
